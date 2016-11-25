5309fbc297b787937850be3ed6fff9c32f227c49
Amr Gaber <amrtgaber@gmail.com>
2016-11-25 02:23:50 -0600

    add better test structure

---
+++ src/cracking-the-coding-interview/chapter-1/1/is-unique.js
@@ -0,0 +1,69 @@
+'use strict';
+
+/**
+ * Implement an algorithm to determine if a string has all unique characters.
+ */
+class IsUnique {
+    /**
+     * Using an array.
+     * Running time: O(n^2)
+     * Space complexity: O(n)
+     * @param  {String} str String to check
+     * @return {boolean}    Returns true if string has all unique characters, false otherwise
+     */
+    usingArray(str) {
+        let strArray = str.split('');
+
+        for( let i = 0; i < str.length; i++ ) {
+            let char = strArray.shift();
+
+            if( strArray.includes(char) ) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Using an array and a hash map.
+     * Running time: O(n)
+     * Space complexity: O(n^2)
+     * @param  {String} str String to check
+     * @return {boolean}    Returns true if string has all unique characters, false otherwise
+     */
+    usingArrayAndMap(str) {
+        let strArray = str.split('');
+        let map = new Map();
+
+        return strArray.every( char => {
+            if( map.has(char) ) {
+                return false;
+            }
+
+            map.set(char, true);
+            return true;
+        });
+    }
+
+    /**
+     * Without additional data structures.
+     * Running time: O(n^2)
+     * Additional Space: O(1)
+     * @param  {String} str String to check
+     * @return {boolean}    Returns true if string has all unique characters, false otherwise
+     */
+    noAdditionalDataStructures(str) {
+        for( let i = 0; i < str.length; i++ ) {
+            for( let j = i + 1; j < str.length; j++ ) {
+                if( str[i] === str[j] ) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
+
+module.exports = exports = new IsUnique();
---
+++ src/cracking-the-coding-interview/chapter-1/1/is-unique.spec.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const expect = require('chai').expect;
+const isUnique = require('./is-unique');
+
+const methods = Object.getOwnPropertyNames( Object.getPrototypeOf(isUnique) ).filter( method => method !== 'constructor' );
+
+const uniqueTestStrings = [
+    '',
+    ' ',
+    'a',
+    'abcdef',
+    'abc def',
+    '\n'
+];
+
+const nonUniqueTestStrings = [
+    'aa',
+    'abcdefa',
+    '  ',
+    ' abcdef ',
+    '\n\n'
+];
+
+describe('Chapter 1.1: Is Unique', function() {
+    methods.forEach( method => {
+        describe(`${method}()`, function() {
+            describe('returns true', function() {
+                uniqueTestStrings.forEach( str => {
+                    it(str, function() {
+                        expect( isUnique[method](str) ).to.be.true;
+                    });
+                });
+            });
+
+            describe('returns false', function() {
+                nonUniqueTestStrings.forEach( str => {
+                    it(str, function() {
+                        expect( isUnique[method](str) ).to.be.false;
+                    });
+                });
+            });
+        });
+    });
+});
