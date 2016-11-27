eb2e6c28c967253b414b616a0c83d6cc973236d2
Amr Gaber <amrtgaber@gmail.com>
2016-11-26 20:51:19 -0600

    add check permutation problem

---
+++ src/cracking-the-coding-interview/chapter-1/2/check-permutation.js
@@ -0,0 +1,53 @@
+'use strict';
+
+/**
+ * Given two strings, determine if one is a permuation of the other.
+ */
+class checkPermutation {
+    /**
+     * Using a map.
+     * Running time: O(n)
+     * Space complexity: O(n)
+     * @param  {String} str1 A string
+     * @param  {String} str2 Another string
+     * @return {boolean}     Returns true if str1 is a permuation of str2, false otherwise
+     */
+    usingMap(str1, str2) {
+        if( str1.length !== str2.length ) {
+            return false;
+        }
+
+        let charCounts = new Map();
+
+        // create map of character counts in str1
+        str1.split('').forEach( char => {
+            if( charCounts.has(char) ) {
+                charCounts.set( char, charCounts.get(char) + 1 );
+            } else {
+                charCounts.set( char, 1 );
+            }
+        });
+
+        // for each character in str2 decrement character count
+        str2.split('').forEach( char => {
+            if( charCounts.has(char) ) {
+                charCounts.set( char, charCounts.get(char) - 1 );
+            } else {
+                // character is not in str1
+                charCounts.set( char, -1 );
+            }
+        });
+
+        // if any counts are < 0 return false
+        for( let count of charCounts.values() ) {
+            if( count < 0 ) {
+                return false;
+            }
+        }
+
+        // str2 contains all characters of str1 the same number of times
+        return true;
+    }
+}
+
+module.exports = exports = new checkPermutation();
---
+++ src/cracking-the-coding-interview/chapter-1/2/check-permutation.spec.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const expect = require('chai').expect;
+const checkPermutation = require('./check-permutation');
+
+const methods = Object.getOwnPropertyNames( Object.getPrototypeOf(checkPermutation) ).filter( method => method !== 'constructor' );
+
+const permutations = [
+    ['', ''],
+    ['a', 'a'],
+    ['abcdefg', 'gfedcba'],
+    ['aaa', 'aaa'],
+    ['1234', '3142'],
+    ['aabbcc', 'abcabc'],
+    ['white space', 'space white'],
+    ['@#$?!', '$#!?@']
+];
+
+const notPermutations = [
+    ['', ' '],
+    ['a', 'b'],
+    ['abcdefg', 'fedcba'],
+    ['aaa', 'aaaa'],
+    ['1234', '7777'],
+    ['aabbcc', 'abcaba'],
+    ['white space', ' spacewhite '],
+    ['@#$?!', '$#!?@!!!!!!!']
+];
+
+describe('Chapter 1.2: Check Permutation', function() {
+    methods.forEach( method => {
+        describe(`${method}()`, function() {
+            describe('returns true', function() {
+                permutations.forEach( pair => {
+                    it(pair.toString(), function() {
+                        expect( checkPermutation[method]( pair[0], pair[1] ) ).to.be.true;
+                    });
+                });
+            });
+
+            describe('returns false', function() {
+                notPermutations.forEach( pair => {
+                    it(pair.toString(), function() {
+                        expect( checkPermutation[method]( pair[0], pair[1] ) ).to.be.false;
+                    });
+                });
+            });
+        });
+    });
+});
