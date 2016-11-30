5c9950cb777fe751f99cb4aab516fea27d597290
Amr Gaber <amrtgaber@gmail.com>
2016-11-29 22:25:34 -0600

    add palindrome permutation

---
+++ src/cracking-the-coding-interview/chapter-1/4/palindrome-permutation.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/**
+ * Check if a string is a permutation of a palindrome.
+ */
+class palindromePermutation {
+    /**
+     * Count chars. A string is a palindrome if there is only 1 character or less
+     * that can be counted an odd number of times.
+     * Running time: O(n)
+     * Space complexity: O(n)
+     * @param  {String} str A string
+     * @return {boolean}    Returns true if string is a permutation of a palindrome, false otherwise
+     */
+    countChars(str) {
+        let charCounts = new Map();
+
+        // count chars
+        str.split('').forEach( char => {
+            if( charCounts.has(char) ) {
+                charCounts.set( char, charCounts.get(char) + 1 );
+            } else {
+                charCounts.set( char, 1 );
+            }
+        });
+
+        // check odd counts
+        let oddCount = 0;
+        for( let count of charCounts.values() ) {
+            if( count % 2 !== 0 ) {
+                oddCount++;
+            }
+
+            if( oddCount > 1 ) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+module.exports = exports = new palindromePermutation();
---
+++ src/cracking-the-coding-interview/chapter-1/4/palindrome-permutation.spec.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const expect = require('chai').expect;
+const palindromePermutation = require('./palindrome-permutation');
+
+const methods = Object.getOwnPropertyNames( Object.getPrototypeOf(palindromePermutation) ).filter( method => method !== 'constructor' );
+
+const palindromes = [
+    '',
+    'a',
+    'aa',
+    'aaa',
+    '1234321',
+    'acacbb',
+    'ttaacco',
+    'jikorkjroei',
+    'abababababab'
+];
+
+const notPalindromes = [
+    'ab',
+    '1234',
+    'ababcd',
+    'abcdefg',
+    'ajbiehg',
+    'baaaaaaac',
+    'baaaacaaaa'
+];
+
+describe('Chapter 1.4: Palindrome Permutation', function() {
+    methods.forEach( method => {
+        describe(`${method}()`, function() {
+            describe('returns true', function() {
+                palindromes.forEach( string => {
+                    it(string, function() {
+                        expect( palindromePermutation[method]( string ) ).to.be.true;
+                    });
+                });
+            });
+
+            describe('returns false', function() {
+                notPalindromes.forEach( string => {
+                    it(string, function() {
+                        expect( palindromePermutation[method]( string ) ).to.be.false;
+                    });
+                });
+            });
+        });
+    });
+});
