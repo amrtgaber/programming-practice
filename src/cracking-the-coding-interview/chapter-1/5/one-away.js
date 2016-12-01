b1530694f0def907e1ad1eca8dc05666d770bcdb
Amr Gaber <amrtgaber@gmail.com>
2016-11-30 23:08:01 -0600

    add one away

---
+++ src/cracking-the-coding-interview/chapter-1/5/one-away.js
@@ -0,0 +1,75 @@
+'use strict';
+
+/**
+ * There are three types of edits: insert a character, remove a character, or
+ * replace a character. Given two strings, write a function to check if they
+ * are one edit or zero edits away.
+ */
+class oneAway {
+    /**
+     * If str2 is 1 length less than str1 check for replacement, if str2 is
+     * 1 length greater than str1 check for insertion, if they are the same length
+     * check for replacement.
+     * Running time: O(n)
+     * Space complexity: O(1)
+     * @param  {String} str A string
+     * @return {boolean}    Returns true if string is a permutation of a palindrome, false otherwise
+     */
+    byLength(str1, str2) {
+        // for each case there should only be 1 edit to find, if there is more than 1 return false
+        let editFound = false;
+        let i = 0;
+        let j = 0;
+
+        if( str1.length === str2.length ) {
+            // must be replacement
+            for( let i = 0; i < str1.length; i++ ) {
+                if( str1[i] !== str2[i] ) {
+                    if( editFound ) {
+                        return false;
+                    }
+
+                    editFound = true;
+                }
+            }
+        } else if( str1.length + 1 === str2.length ) {
+            // must be insert
+            while( i < str1.length && j < str2.length ) {
+                if( str1[i] !== str2[j] ) {
+                    if( editFound ) {
+                        return false;
+                    }
+
+                    editFound = true;
+
+                    j++;
+                } else {
+                    i++;
+                    j++;
+                }
+            }
+        } else if( str1.length - 1 === str2.length ) {
+            // must be remove
+            while( i < str1.length && j < str2.length ) {
+                if( str1[i] !== str2[j] ) {
+                    if( editFound ) {
+                        return false;
+                    }
+
+                    editFound = true;
+                    i++;
+                } else {
+                    i++;
+                    j++;
+                }
+            }
+        } else {
+            // more than one edit away
+            return false;
+        }
+
+        return true;
+    }
+}
+
+module.exports = exports = new oneAway();
---
+++ src/cracking-the-coding-interview/chapter-1/5/one-away.spec.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const expect = require('chai').expect;
+const oneAway = require('./one-away');
+
+const methods = Object.getOwnPropertyNames( Object.getPrototypeOf(oneAway) ).filter( method => method !== 'constructor' );
+
+const oneAwayStrings = [
+    ['', ''],
+    ['a', 'a'],
+    ['abc', 'aac'],
+    ['abc', 'bbc'],
+    ['abc', 'aba'],
+    ['abcd', 'abd'],
+    ['abcd', 'abc'],
+    ['abcd', 'bcd'],
+    ['abcd', 'abcde'],
+    ['abcd', 'eabcd'],
+    ['abcd', 'abecd']
+];
+
+const notOneAwayStrings = [
+    ['', 'aa'],
+    ['abc', 'aaa'],
+    ['abc', 'bbb'],
+    ['abc', 'a'],
+    ['abcd', 'aabd'],
+    ['abcd', 'abcdef'],
+    ['abcd', 'efbcd'],
+    ['abcd', 'ae'],
+    ['abcd', 'ad'],
+    ['abcd', 'abecde']
+];
+
+describe('Chapter 1.5: One Away', function() {
+    methods.forEach( method => {
+        describe(`${method}()`, function() {
+            describe('returns true', function() {
+                oneAwayStrings.forEach( pair => {
+                    it(pair.toString(), function() {
+                        expect( oneAway[method]( pair[0], pair[1] ) ).to.be.true;
+                    });
+                });
+            });
+
+            describe('returns false', function() {
+                notOneAwayStrings.forEach( pair => {
+                    it(pair.toString(), function() {
+                        expect( oneAway[method]( pair[0], pair[1] ) ).to.be.false;
+                    });
+                });
+            });
+        });
+    });
+});
