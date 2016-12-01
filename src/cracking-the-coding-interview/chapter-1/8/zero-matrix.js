0c408d239fe44c261adb9f0c6aeb84a1c9602679
Amr Gaber <agaber@kineticsupply.com>
2016-12-01 11:40:45 -0600

    add zero matrix problem

---
+++ src/cracking-the-coding-interview/chapter-1/8/zero-matrix.js
@@ -0,0 +1,49 @@
+'use strict';
+
+/**
+ * Write an algorithm such that if an element in an MxN matrix is 0, its entire
+ * row and column are set to 0.
+ */
+class zeroMatrix {
+    /**
+     * Basic implementation
+     * Running time: O(m*n)
+     * Space complexity: O(m*n)
+     * @param {Array} matrix An MxN matrix
+     */
+    basic(matrix) {
+        let m = matrix.length;
+        let n = matrix[0].length;
+
+        let zeroRows = [];
+        let zeroCols = [];
+
+        // find all zeroes
+        for( let i = 0; i < m; i++ ) {
+            for( let j = 0; j < n; j++ ) {
+                if( matrix[i][j] === 0 ) {
+                    zeroRows.push(i);
+                    zeroCols.push(j);
+                }
+            }
+        }
+
+        // zero out all rows
+        zeroRows.forEach( row => {
+            for( let col = 0; col < n; col++ ) {
+                matrix[row][col] = 0;
+            }
+        });
+
+        // zero out all cols
+        zeroCols.forEach( col => {
+            for( let row = 0; row < m; row++ ) {
+                matrix[row][col] = 0;
+            }
+        });
+
+        return matrix;
+    }
+}
+
+module.exports = exports = new zeroMatrix();
---
+++ src/cracking-the-coding-interview/chapter-1/8/zero-matrix.spec.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const expect = require('chai').expect;
+const zeroMatrix = require('./zero-matrix');
+
+const methods = Object.getOwnPropertyNames( Object.getPrototypeOf(zeroMatrix) ).filter( method => method !== 'constructor' );
+
+const tests = require('./zero-matrix-tests');
+
+describe('Chapter 1.6: Zero Matrix', function() {
+    methods.forEach( method => {
+        describe(`${method}()`, function() {
+            tests.forEach( pair => {
+                let testTitle = '\n';
+                for( let i = 0; i < pair[0].length; i++ ) {
+                    testTitle += `\t${pair[0][i]}\t${pair[1][i]}\n`;
+                }
+
+                it(testTitle, function() {
+                    expect( zeroMatrix[method]( pair[0] ) ).to.deep.equal( pair[1] );
+                });
+            });
+        });
+    });
+});
